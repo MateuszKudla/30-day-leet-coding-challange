0f86496e2673174d169407786a88126a50fa5d2b
Mateusz Kudła <mat.kudla@gmail.com>
2020-04-02 09:31:58 +0200

    Add solution for day 2

---
+++ 2-day/happy-number.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        slow  = n
+        fast = n
+        
+        while True:
+            slow = self.calculateSquareSum(slow)
+            fast = self.calculateSquareSum(self.calculateSquareSum(fast))
+
+            if slow == fast:
+                break
+            
+        return slow == 1
+
+
+    def calculateSquareSum(self, n: int) -> int:
+        squareSum = 0
+        while n != 0:
+            digit = n % 10
+            n //= 10
+            squareSum += digit**2
+
+        return squareSum
+
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    result = solution.isHappy(15)
+    print (result)
+        
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Challange takes place on [leetcode.com](https://leetcode.com/) and runs from 1 t
 | Day | Title | Solution |
 | --- | ----- | -------- |
 | 1 | [Single number](https://leetcode.com/problems/single-number/) | [Python](https://github.com/MateuszKudla/30-day-leet-coding-challange/blob/master/1-day/single-number.py) | 
+| 2 | [Happy number](https://leetcode.com/problems/happy-number/) | [Python]((https://github.com/MateuszKudla/30-day-leet-coding-challange/blob/master/2-day/happy-number.py) |
